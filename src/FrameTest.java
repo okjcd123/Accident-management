c26f8b0dae7c387a9031bffa02df70ef0ae61b7a
Junhyuk Kim <okjcd123@naver.com>
2017-12-24 16:25:59 +0900

    [add] FrameTest.java

---
+++ src/FrameTest.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class FrameTest extends JFrame{
+
+	public FrameTest()
+	{
+		setSize(1000,1000);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String [] args)
 	{
 		System.out.println("Accident Management System");
+		new FrameTest();
 	}
 }
