307bd506877023e65c9199bd17c33b1fc84d8832
HeehoMoon <heeho9307@gmail.com>
2017-12-25 22:32:38 +0900

    [add] Car class, Police class, AccidentCase class 추가

---
+++ src/Police.java
@@ -0,0 +1,35 @@
+
+public class Police {
+
+	private String policeno;
+	private String polname;
+	private String rank;
+	private String depart;
+	
+	public String getPoliceno() {
+		return policeno;
+	}
+	public void setPoliceno(String policeno) {
+		this.policeno = policeno;
+	}
+	public String getPolname() {
+		return polname;
+	}
+	public void setPolname(String polname) {
+		this.polname = polname;
+	}
+	public String getRank() {
+		return rank;
+	}
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+	public String getDepart() {
+		return depart;
+	}
+	public void setDepart(String depart) {
+		this.depart = depart;
+	}
+	
+}
+
