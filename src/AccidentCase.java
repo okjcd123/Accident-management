2c1e138632971be13c012aa1bcacf94570deffbb
HeehoMoon <heeho9307@gmail.com>
2017-12-28 00:21:35 +0900

    [update] AccidentCase 클래스 인스턴스 자료형 수정 및 삭제

--- src/AccidentCase.java
+++ src/AccidentCase.java
@@ -1,20 +1,19 @@
 
 public class AccidentCase {
 
-	private int cscode;
-	private String province;
-	private String town;
-	private String year;
-	private String month;
-	private String day;
-	private String policeno;
-	private String carno;
-	private int casulity; 
-	private int dead;
-	private int injured;
-	private String actype;
-	private float latitude;
-	private float longitude;
+	private int cscode;   			//사고 번호
+	private String province;        //시,도
+	private String town;            //군,구
+	private String year;            //년도
+	private String month;           //월
+	private String day;             //일
+	private String policeno;        //경찰번호
+	private int casulity;           //사상자 = 사망자 + 부상자
+	private int dead;               //사망자
+	private int injured;            //부상자 
+	private String actype;          //사고유형 (차대차, 차대사람, 차량단독)
+	private double latitude;        //위도
+	private double longitude;       //경도 
 	
 	public int getCscode() {
 		return cscode;
@@ -58,17 +57,11 @@ public class AccidentCase {
 	public void setPoliceno(String policeno) {
 		this.policeno = policeno;
 	}
-	public String getCarno() {
-		return carno;
-	}
-	public void setCarno(String carno) {
-		this.carno = carno;
-	}
 	public int getCasulity() {
 		return casulity;
 	}
-	public void setCasulity(int casulity) {
-		this.casulity = casulity;
+	public void setCasulity() {
+		this.casulity = this.dead + this.injured;
 	}
 	public int getDead() {
 		return dead;
@@ -88,19 +81,18 @@ public class AccidentCase {
 	public void setActype(String actype) {
 		this.actype = actype;
 	}
-	public float getLatitude() {
+	public double getLatitude() {
 		return latitude;
 	}
-	public void setLatitude(float latitude) {
+	public void setLatitude(double latitude) {
 		this.latitude = latitude;
 	}
-	public float getLongitude() {
+	public double getLongitude() {
 		return longitude;
 	}
-	public void setLongitude(float longitude) {
+	public void setLongitude(double longitude) {
 		this.longitude = longitude;
 	}
-	
 	
 	
 }
