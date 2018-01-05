5155506122e543419c7b49a04866b04804133585
HeehoMoon <heeho9307@gmail.com>
2018-01-05 22:27:04 +0900

    [add] AccidentCaseDAO 클래스 주석 추가 및 미사용 메소드 삭제

--- src/AccidentCaseDAO.java
+++ src/AccidentCaseDAO.java
@@ -3,7 +3,7 @@
  * @author 김준혁, 김준혁, 문희호
  * 
  * 최종 작성일: 2017년 12월 23일
- * 최종 수정일: 2018년 1월 2일
+ * 최종 수정일: 2018년 1월 5일
  */
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -11,43 +11,45 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.*;
-import javax.swing.JTable;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 import org.json.simple.parser.JSONParser;
 public class AccidentCaseDAO {
 	
-	String DBid;
-	String DBpw;
+	String DBid;     // MySQL username
+	String DBpw;     // MySQL password
 	
-	String jdbcDriver = "com.mysql.jdbc.Driver";
-	String jdbcUrl = "jdbc:mysql://localhost/javadb";
-	
+	String jdbcDriver = "com.mysql.jdbc.Driver";      // MySQL jdbc 드라이버
+	String jdbcUrl = "jdbc:mysql://localhost/javadb"; // MySQL javadb schema    
+
 	Connection conn;
+	String sql;   		    	// SQL 문
 	PreparedStatement pstmt;
-	ResultSet rs;
+	ResultSet rs;  				// SQL문 실행 결과로 반환되는 튜플을 저장
 
-	String sql;
-	ArrayList<AccidentCase> datas;
+	
+	ArrayList<AccidentCase> datas;  // AccidentCase 클래스 반환하는 ArrayList
 
+	// 인스턴스를 생성하여 AppManager 클래스에 저장하는 메소드
 	public AccidentCaseDAO()
 	{
 		AppManager.CreateInstance().setAccidentCaseDAO(this);
 	}
 	
+	// DB(MySQL)의 username과 password가 일치하는지 확인하는 메소드
 	boolean connectTest(String id, String pw)
 	{
 		try {
-			Class.forName(jdbcDriver);
-			DriverManager.getConnection(jdbcUrl,id,pw);
 			this.DBid= id;
 			this.DBpw = pw;
+			
+			Class.forName(jdbcDriver);
+			DriverManager.getConnection(jdbcUrl,DBid,DBpw);
+		
 			return true;
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
@@ -59,9 +61,8 @@ public class AccidentCaseDAO {
 		}
 	}
 
-	//DB연결
+	// DB와 연결하는 메소드
 	void connectDB(){
-
 		try {
 			Class.forName(jdbcDriver);
 			conn = DriverManager.getConnection(jdbcUrl,DBid,DBpw);
@@ -74,7 +75,7 @@ public class AccidentCaseDAO {
 		}
 	}
 
-	//DB 닫기
+	//DB와 연결을 종료하는 메소드
 	void closeDB(){
 		try {
 			pstmt.close();
@@ -85,9 +86,10 @@ public class AccidentCaseDAO {
 		}
 	}
 	
+	// 새로 추가된 사고 번호를 반환 하는 메소드(현재 DB에 존재하는 가장 큰 사고번호 반환)
 	int getNewCaseCode() {
 		
-		connectDB();
+		connectDB();   //DB 연결
 		int newCsCode = 0;
 		
 		sql = "select MAX(cscode) from accidentcase";
@@ -105,20 +107,20 @@ public class AccidentCaseDAO {
 			e.printStackTrace();
 		}
 		
-		closeDB();
-		
+		closeDB();    //DB 연결 닫기
 		return newCsCode;
 	}
 	
+	// 사고 번호를 전달 받아 해당하는 AccidentCase를 반환하는 메소드 
 	AccidentCase getCase(int cscode) 
 	{
-		connectDB();
+		connectDB();    //DB 연결
 		AccidentCase accCase = new AccidentCase();
 		sql = "select * from accidentcase where cscode = ?";
 		try {
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, cscode);
-			rs = pstmt.executeQuery();
+			pstmt.setInt(1, cscode);         
+			rs = pstmt.executeQuery();       
 
 			if(rs.next())
 			{
@@ -136,20 +138,23 @@ public class AccidentCaseDAO {
 				accCase.setLongitude(rs.getDouble("longitude"));
 			}
 			
-			rs.close();
+			rs.close(); // resultSet 닫기
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		closeDB();
+		closeDB();   //DB 연결 닫기
 		return accCase;
 	}
 	
+	// 시/도, 군/구, 년, 월을 전달 받아 조건에 맞는 ArrayList를 반환하는 메소드 
 	ArrayList<AccidentCase> searchCase(String province, String town, String year, String month)
 	{
 		AccidentCase accCase;
 		datas = new ArrayList<AccidentCase>();
-		connectDB();
+		connectDB(); //DB 연결
+		
 		sql = "select * from accidentcase where (province = ? and town = ? and year = ? and month = ?)";
 		
 		try {
@@ -178,22 +183,23 @@ public class AccidentCaseDAO {
 				datas.add(accCase);
 			}
 			
-			rs.close();
+			rs.close();   //resultSet 닫기
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		closeDB();
+		closeDB();    // DB 연결 닫기
 		return datas;
 	}	
 
+	// 시/도, 군/구, 년을 전달 받아 조건에 맞는 ArrayList를 반환하는 메소드 
 	ArrayList<AccidentCase> searchCaseLocaYear(String province, String town, String year)
 	{
 		AccidentCase accCase;
 		datas = new ArrayList<AccidentCase>();
-		connectDB();
+		connectDB();   //DB 연결
 		sql = "select * from accidentcase where (province = ? and town = ? and year = ?)";
 		
 		try {
@@ -221,22 +227,23 @@ public class AccidentCaseDAO {
 				datas.add(accCase);
 			}
 			
-			rs.close();
+			rs.close();   // resultSet 닫음
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		closeDB();
+		closeDB();        //DB 연결 닫기
 		return datas;
 	}
 	
+	// 시/도, 군/구, 년, 월을 전달 받아 조건에 맞는 ArrayList를 반환하는 메소드 
 	ArrayList<AccidentCase> searchCaseLocaMonth(String province, String town, String month)
 	{
 		AccidentCase accCase;
 		datas = new ArrayList<AccidentCase>();
-		connectDB();
+		connectDB(); //DB 연결 
 		sql = "select * from accidentcase where (province = ? and town = ? and month = ?)";
 		
 		try {
@@ -264,35 +271,38 @@ public class AccidentCaseDAO {
 				datas.add(accCase);
 			}
 			
-			rs.close();
+			rs.close();           // resultSet 닫음
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		closeDB();
+		closeDB();                // DB 연결 종료  
 		return datas;
 	}
 	
+	// 시/도, 군/구을 전달 받아 조건에 맞는 ArrayList를 반환하는 메소드 
 	ArrayList<AccidentCase> searchCaseLoca(String province, String town)
 	{
 		AccidentCase accCase;
-		datas = new ArrayList<AccidentCase>();
-		connectDB();
+		datas = new ArrayList<AccidentCase>(); 
+		
+		connectDB();   // DB 연결
 		sql = "select * from accidentcase where (province = ? and town = ?)";
 		
 		try {
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, province);
-			pstmt.setString(2, town);
-			rs = pstmt.executeQuery();
-		
-
+			pstmt = conn.prepareStatement(sql);   
+			pstmt.setString(1, province);         // 첫 번째 물음표 위치에 province 지정 
+			pstmt.setString(2, town);             // 두 번째 물음표 위치에 town 지정
+			rs = pstmt.executeQuery();            
+	
 			while(rs.next()) {
-				accCase = new AccidentCase();
-				accCase.setCscode(rs.getInt("cscode"));
-				accCase.setProvince(rs.getString("province"));
+				// AccidentCase객체 생성
+				accCase = new AccidentCase();   
+				// 실행 결과로 반환된 행을 AccidentCase 객체의 변수에 지정 
+				accCase.setCscode(rs.getInt("cscode"));              
+				accCase.setProvince(rs.getString("province"));       
 				accCase.setTown(rs.getString("town"));
 				accCase.setYear(rs.getString("year"));
 				accCase.setMonth(rs.getString("month"));
@@ -303,26 +313,27 @@ public class AccidentCaseDAO {
 				accCase.setActype(rs.getString("actype"));
 				accCase.setLatitude(rs.getDouble("latitude"));
 				accCase.setLongitude(rs.getDouble("longitude"));
-				
+				// ArrayList에 AccidentCase 추가
 				datas.add(accCase);
 			}
 			
-			rs.close();
+			rs.close(); //resultSet 닫기
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		closeDB();
+		closeDB();   // DB 연결 닫음 
 		return datas;
 	}	
 	
+	//년, 월을 전달 받아 조건에 맞는 ArrayList를 반환하는 메소드 
 	ArrayList<AccidentCase> searchCaseTime(String year, String month)
 	   {
 	      AccidentCase accCase;
 	      datas = new ArrayList<AccidentCase>();
-	      connectDB();
+	      connectDB();    // DB 연결 
 	      
 	         sql = "select * from accidentcase where (year = ? and month = ?)";
 	         try {
@@ -348,18 +359,23 @@ public class AccidentCaseDAO {
 	               
 	               datas.add(accCase);
 	            }
+	            
+	            rs.close();   // resultSet 닫음 
 	         } catch (SQLException e) {
 	            // TODO Auto-generated catch block
 	            e.printStackTrace();
 	         }
+	         
+	         closeDB(); // DB 연결 닫음
 	         return datas;  
 	      }
 		
+	//년을 전달 받아 조건에 맞는 AccidentCase를 ArrayList에 담아 반환하는 메소드 	
 	ArrayList<AccidentCase> searchCaseTime(String year)
 	{
 		AccidentCase accCase;
 		datas = new ArrayList<AccidentCase>();
-		connectDB();
+		connectDB();    // DB 연결 
 		
 	      sql = "select * from accidentcase where year = ?";
 	      try {
@@ -386,21 +402,23 @@ public class AccidentCaseDAO {
 					datas.add(accCase);
 	         }
 	         
-	     	rs.close();
+	     	rs.close();  //resultSet 닫음
 	      } catch (SQLException e) {
 	         // TODO Auto-generated catch block
 	         e.printStackTrace();
 	      }
+	      closeDB(); // DB 연결 닫음
 	      return datas;
 	   }
-	
+
+	//월을 전달 받아 조건에 맞는 AccidentCase를 ArrayList에 담아 반환하는 메소드 	
 	ArrayList<AccidentCase> searchCaseMonthTime(String month)
 	   {
 	      AccidentCase accCase;
 	      datas = new ArrayList<AccidentCase>();
-	      connectDB();
+	      connectDB();    // DB 연결 
 	      
-	         sql = "select * from accidentcase where month = ?)";
+	         sql = "select * from accidentcase where month = ?";
 	         try {
 	            pstmt = conn.prepareStatement(sql);
 	            pstmt.setString(1,month);
@@ -423,19 +441,24 @@ public class AccidentCaseDAO {
 	               
 	               datas.add(accCase);
 	            }
+	            rs.close();  // resultSet 닫음
 	         } catch (SQLException e) {
 	            // TODO Auto-generated catch block
 	            e.printStackTrace();
 	         }
+	         
+	         closeDB();   // DB 연결 닫음
 	         return datas;  
 	      }
 
-	
+	// DB에 존재하는 모든 사고 사례를 ArrayList에 담아 반환하는 메소드 
 	ArrayList<AccidentCase> getAll() {
 		
 		AccidentCase accCase;
 		datas = new ArrayList<AccidentCase>();
-		connectDB();
+		
+		connectDB();  // DB 연결
+		
 		sql = "select * from accidentcase";
 		try {
 			pstmt = conn.prepareStatement(sql);
@@ -459,20 +482,21 @@ public class AccidentCaseDAO {
 				datas.add(accCase);
 			}
 			
-			rs.close();
+			rs.close();  // resultSet 닫음
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		closeDB();
+		closeDB();   // DB 연결 닫음
 		return datas;
 	}
 	 
+	//AccidentCase를 전달받아 DB에 삽입하는 메소드  	
 	 boolean insertCase(AccidentCase accCase) 
 		{
-			connectDB();
+			connectDB();     // DB 연결
 			int chk =0;
 			boolean flag=false; 
 			sql = "INSERT "+
@@ -495,7 +519,7 @@ public class AccidentCaseDAO {
 				
 				chk = pstmt.executeUpdate();
 				
-				if(chk >0)
+				if(chk >0)   // 변화시킨 행의 수가 존재할 때 
 					flag = true;
 				
 			} catch (SQLException e) {
@@ -503,15 +527,16 @@ public class AccidentCaseDAO {
 				e.printStackTrace();
 			}
 
-			closeDB();
+			closeDB();    // DB 연결 종료
 			return flag;
 		}
 		
+		//AccidentCase를 전달받아 DB에 존재하는 정보를 수정하는 메소드	
 		boolean updateCase(AccidentCase accCase) 
 		{
-			connectDB();
+			connectDB();   // DB 연결
 			int chk;
-			boolean flag=false; 
+			boolean flag = false; 
 			
 			sql = "UPDATE accidentcase " + 
 				  "SET province = ? , town = ?, year = ? , month = ?, day = ?, dead = ?, injured =? , actype = ?, latitude = ?, longitude = ? " + 
@@ -533,7 +558,7 @@ public class AccidentCaseDAO {
 				
 				chk = pstmt.executeUpdate();
 				
-				if(chk>0)
+				if(chk>0)   // 변화시킨 행의 수가 존재할 때 
 					flag = true;
 				
 			} catch (SQLException e) {
@@ -541,42 +566,43 @@ public class AccidentCaseDAO {
 				e.printStackTrace();
 			}
 
-			closeDB();
+			closeDB();     // DB 종료
 			return flag;
 		}
 
+		// cscode를 전달받아 해당하는 데이터 삭제하는 메소드
 		boolean deleteCase(int cscode)
 		{
-			connectDB();
+			connectDB();    // DB 연결
 			sql = "delete from accidentcase where cscode = ?";
 			int chk;
 			boolean flag = false;
 			
 			try {
 				pstmt = conn.prepareStatement(sql);
-				pstmt.setInt(1, cscode);
-				chk = pstmt.executeUpdate();
+				pstmt.setInt(1, cscode);   			// 첫 번째 물음표에 cscode 지정
+				chk = pstmt.executeUpdate();        // pstmt 실행
 				
-				if(chk>0)
+				if(chk>0) // 변화 시킨 행의 수가 존재할 때
 					flag = true;
 				
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			closeDB();
+			closeDB();      // DB 종료
 			return flag;
 		}
 		
+		// DB에 존재하는 모든 데이터를 삭제하는 메소드
 		void deleteAllCase()
 		{
-			connectDB();
+			connectDB();        // DB 연결
 			sql = "TRUNCATE accidentcase;";
-			
-			try {
-				Statement stmt = conn.createStatement();
-				stmt.executeUpdate(sql);
 
+			try {
+				pstmt = conn.prepareStatement(sql);
+				pstmt.executeUpdate();        // pstmt 실행
 				
 			} catch (SQLException e) 
 			{
@@ -584,17 +610,20 @@ public class AccidentCaseDAO {
 				e.printStackTrace();
 			}
 			
-			closeDB();
-
+			closeDB();         // DB 종료
 		}
 		
+		//년도를 입력받아 해당하는 데이터 파싱하는 메소드
 		boolean ParsingAccidentData(String year)
 		{
 			// JSON 데이터가 저장된 url 주소를 저장하는 String 배열
 			String url[] = new String[3];
 			
+			//서울
 			url[0] = "http://apis.data.go.kr/B552061/trafficAccidentDeath/getRestTrafficAccidentDeath?servicekey=2%2BbS76NE8TFgiPqTxDyXneu97RYUJJxFo3c1K5FHwD5tja3W8etlkdp6jXOr9zHl6xAZJB9LUaLrcEudogq5iQ%3D%3D&searchYear="+year+"&siDo=1100&guGun=";
+			//경기
 			url[1] = "http://apis.data.go.kr/B552061/trafficAccidentDeath/getRestTrafficAccidentDeath?servicekey=2%2BbS76NE8TFgiPqTxDyXneu97RYUJJxFo3c1K5FHwD5tja3W8etlkdp6jXOr9zHl6xAZJB9LUaLrcEudogq5iQ%3D%3D&searchYear="+year+"&siDo=1300&guGun=";
+			//인천
 			url[2] = "http://apis.data.go.kr/B552061/trafficAccidentDeath/getRestTrafficAccidentDeath?servicekey=2%2BbS76NE8TFgiPqTxDyXneu97RYUJJxFo3c1K5FHwD5tja3W8etlkdp6jXOr9zHl6xAZJB9LUaLrcEudogq5iQ%3D%3D&searchYear="+year+"&siDo=2300&guGun=";
 
 			// Parsing을 진행할 Parser 생성
@@ -686,7 +715,8 @@ public class AccidentCaseDAO {
 						
 		}
 		
-		String findProvince(String code)
+		// 시/도 코드를 전달받아 해당하는 시/도 문자열을 반환하는 메소드
+		private String findProvince(String code)
 		{
 			String str[] = {"서울특별시","경기도","인천광역시"};
 			
@@ -700,7 +730,8 @@ public class AccidentCaseDAO {
 				return "NULL";
 		}
 		
-		String findTown(String code)
+		// 군/구 코드를 전달받아 해당하는 군/구 문자열을 반환하는 메소드 
+		private String findTown(String code)
 		{
 			String seoul[] = {"종로구","중구","용상구","성동구","동대문구","성북구","도봉구","은평구","서대문구","마포구","강서구","구로구","영등포구","동작구",
 					"관악구","강남구","강동구","송파구","서초구","양천구","중랑구","노원구","광진구","강북구","금천구"};
@@ -729,39 +760,8 @@ public class AccidentCaseDAO {
 			
 		}
 		
-		String findCode(String province, String town)
-		{
-			String seoul[] = {"종로구","중구","용상구","성동구","동대문구","성북구","도봉구","은평구","서대문구","마포구","강서구","구로구","영등포구","동작구",
-					"관악구","강남구","강동구","송파구","서초구","양천구","중랑구","노원구","광진구","강북구","금천구"};
-			
-			String kyenggi[] = {"수원시","성남시", "의정부시","안양시","부천시","안산시",	"평택시",	"광명시",	"구리시",	"양주시",	"남양주군","여주시","평택군","화성시","시흥시","파주시","고양시","광주시",
-					"연천군","포천시","가평군","양평군","이천시","용인시","안성시","김포시","강화군","옹진군","동두천시","송탄시","과천시","군포시","남양주시","오산시","의왕시","하남시"};
-
-			String incheon[] = {"중구","동구","남구","부평구","남동구","서구","연수구","계양구","강화군","옹진군"	};
-			
-			if(province.equals("서울특별시"))
-			{
-				for(int i = 0; i<seoul.length; i++)
-					if(seoul[i].equals(town))
-						return String.valueOf(i+1101);
-			}
-			else if(province.equals("경기도"))
-			{
-				for(int i = 0; i<kyenggi.length; i++)
-					if(kyenggi[i].equals(town))
-						return String.valueOf(i+1302);
-			}
-			else if(province.equals("인천광역시"))
-			{
-				for(int i = 0; i<incheon.length; i++)
-					if(incheon[i].equals(town))
-						return String.valueOf(i+2301);
-			}
-			
-			return "NULL";
-		}
-				
-		String findAcctype(String code)
+		// 차량 사고 타입 코드를 전달받아 해당하는 사고타입 문자열 반환하는 메소드
+		private String findAcctype(String code)
 		{
 			String acctype[] = {"차대사람","차대차","차량단독"};
 			
@@ -774,10 +774,5 @@ public class AccidentCaseDAO {
 			else 
 				return "NULL";
 		}
-			
-			
-
-
-
 
 }
